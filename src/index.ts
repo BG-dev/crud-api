20df87226242c7f19e3c47ebdfc73c26a7389a94
nikita.azizov <nik.azizoff2016@gmail.com>
2024-10-22 01:36:43 +0300

    feat: add requestHandler

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import http from 'http';
+import dotenv from 'dotenv';
+import { requestHandler } from './services/requestHandler';
+
+dotenv.config();
 
 const port: number = Number(process.env.PORT) || 4000;
 
-const server = http.createServer();
+const server = http.createServer(requestHandler);
 
 server.listen(port, () => console.log(`SERVER IS RUNNING ON PORT: ${port}`));
